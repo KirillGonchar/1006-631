3c5e8193c627b7202393981524e7df11b10b9de1
KirillGonchar <gonchar.k.u@gmail.com>
2015-10-06 19:11:29 +0300

    22

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int m = 55;
-        int n = 70;
-        int m1 = 10 - m;
-        int n1 = 10 - n;
+        int m = 3430;
+        int n = 1130;
+        int m1 = Math.abs(10 - m);
+        int n1 = Math.abs(10 - n);
 
-        if (m1 < n1){
+        if (m1 <= n1){
         System.out.println(m);}
         else {
             System.out.println(n);
