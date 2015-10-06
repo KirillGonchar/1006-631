ba2119cc3f04c30970146cd9fe11a3310ca00ef0
KirillGonchar <gonchar.k.u@gmail.com>
2015-10-06 18:42:21 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 /**
  * Created by kirill on 10/6/2015.
  */
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) {
+        int m = 55;
+        int n = 70;
+        int m1 = 10 - m;
+        int n1 = 10 - n;
+
+        if (m1 < n1){
+        System.out.println(m);}
+        else {
+            System.out.println(n);
+        }
+
+    }
 }
